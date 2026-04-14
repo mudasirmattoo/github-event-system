4b9e597edc70d9bfa07533dd111af96f61df4211
mudasirmattoo <mudasirmmattoo@gmail.com>
2026-04-14 12:30:58 +0530

    different event parameters added

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +14,57 @@ func main() {
 
 		eventType := c.GetHeader("X-GitHub-Event")
 
+		deliveryID := c.GetHeader("X-Github-Delivery")
+
 		body, err := c.GetRawData()
 		if err != nil {
 			c.JSON(400, gin.H{"error": "Cannot read body"})
 			return
 		}
 
+		var payload map[string]interface{}
+		if err := json.Unmarshal(body, &payload); err != nil {
+			log.Println("Error parsing JSON:", err)
+			return
+		}
+
+		// Repository info
+		repoData := payload["repository"].(map[string]interface{})
+		repoName := repoData["name"]
+		fullRepo := repoData["full_name"]
+
+		// Branch (ref = refs/heads/main)
+		branch := payload["ref"]
+
+		// User info
+		pusherData := payload["pusher"].(map[string]interface{})
+		pusher := pusherData["name"]
+
+		// Commit info (take first commit for now)
+		commits := payload["commits"].([]interface{})
+
+		var commitID, message, timestamp interface{}
+
+		if len(commits) > 0 {
+			firstCommit := commits[0].(map[string]interface{})
+			commitID = firstCommit["id"]
+			message = firstCommit["message"]
+			timestamp = firstCommit["timestamp"]
+		}
+
+		compareURL := payload["compare"]
+
+		log.Println("----- New Event -----")
+		log.Println("Delivery ID:", deliveryID)
 		log.Println("Event Type:", eventType)
-		log.Println("Raw Body:", string(body))
+		log.Println("Repo:", repoName)
+		log.Println("Full Repo:", fullRepo)
+		log.Println("Branch:", branch)
+		log.Println("Pushed by:", pusher)
+		log.Println("Commit ID:", commitID)
+		log.Println("Message:", message)
+		log.Println("Timestamp:", timestamp)
+		log.Println("Compare URL:", compareURL)
 
 		c.JSON(200, gin.H{
 			"message": "Webhook received",
